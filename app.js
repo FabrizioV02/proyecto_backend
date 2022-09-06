3237de65bd30e53429141734f6993ded1a9b4397
Fabrizio <fabriziovaldovinos162@gmail.com>
2022-09-05 21:48:09 -0300

    Manejo de archivos JS

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
-class Usuario {
-    constructor(name, surname, books, pet){
-        this.userName = name;
-        this.userLastname = surname;
-        this.userBooks = books;
-        this.userPet = pet;
+const fs = require('fs');
+
+class Container {
+    constructor(name){
+        this.name = name;
     }
 
-    getFullName (){
-        return console.log(`Hola ${this.userName} ${this.userLastname}.`);
+    async getAll() {
+        try {
+            let content = await fs.promises.readFile(`./${this.name}`,'utf-8');
+            content = JSON.parse(content)
+            return content
+        }
+        catch (err) {
+            return err
+        }
+
     }
 
-    addPet(newPet){
-        this.userPet.push(newPet);
+
+    async saveInfo(info){
+        try {
+            const content = await this.getAll();
+            content.map(obj => {if (obj.id == info.id) throw new Error ('ID existente')}) // Error si encuentra IDs duplicadas
+            content.push(info);
+            await fs.promises.writeFile(`./${this.name}`, JSON.stringify(content));
+            return `Objeto cargado correctamente. ID: ${info.id}`
+        }
+        catch (err) {
+            console.log(`Error: ${err}`)
+        }
     }
 
-    countPets(){
-        let i = 0;
-        this.userPet.forEach(element => ++i);
-        return console.log(`Usted tiene ${i} mascotas.`);
+    async getById(id) {
+        try {
+            const content = await this.getAll();
+            let response;
+            content.map(obj => {(obj.id == id) ? response = obj : response = 'Este ID no fue encontrado'});
+            return response;
+        }
+        catch (err){
+            return err
+        }
     }
- 
-    getBookNames(){
-        this.userBooks.forEach(element => console.log(element.title));
+
+    async deleteById(id) {
+        try {
+            const content = await this.getAll();
+            const newContent = content.filter(e => {e.id !== id})
+            await fs.promises.writeFile(`./${this.name}`, JSON.stringify(newContent));
+            return newContent;
+        }
+        catch (err){
+            return err
+        }
     }
 
-};
+    async deleteAll() {
+        try {
+            const content = await this.getAll();
+            await fs.promises.writeFile(`./${this.name}`, JSON.stringify([]));
+            return console.log("contenido eliminado");
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }
 
-const user = new Usuario('Enrique','Lopez',[{title : 'The Mist', author : 'Stephen King'},{title : 'Star Wars', author : 'George Lucas'}], ['Gato','Foca']);
-const user2 = new Usuario('Gabriel','Sanchez',[{title : 'IT', author : 'Stephen King'},{title : 'Dune', author : 'Frank Herbert'}], ['Leon','Nutria']);
+    
+}
 
-console.log(user);
-console.log(user2);
-//user 1 
-user.getFullName();
-user.addPet('Athos');
-user.countPets();
-console.log( `${user.userName} tiene estos libros:` );
-user.getBookNames();
 
-console.log(user);
-//user 2
-user2.getFullName();
-user2.addPet('Ardilla');
-user2.addPet('Lobo marino');
-user2.addPet('Iguana');
-user2.countPets();
-console.log( `${user2.userName} tiene estos libros:` );
-user2.getBookNames();
-console.log(user2);
+
+const producto = {
+    id:3,
+    name: "Campera de cuero",
+    stock: 10,
+    price: 900.0,
+  };
+
+
+
+const contenedor = new Container("productos.json");
+
+
+contenedor.saveInfo(producto).then((res) => console.log(res)); // Carga la Informacion nueva
+
+// contenedor.getById(2).then((res) => console.log(res));  // Buscar por su ID
+
+// contenedor.getAll().then((res) => console.log(res)); // Obtiene todo el objeto
+
+// contenedor.deleteById(1).then((res) => console.log(res));
+
+// contenedor.deleteAll().then((res) => console.log(res));
