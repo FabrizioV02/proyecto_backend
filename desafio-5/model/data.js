91031f87865660051636376b03f39c44f2ac27b7
Fabrizio <fabriziovaldovinos162@gmail.com>
2022-10-03 21:37:16 -0300

    WebSocket

--- desafio-5/model/data.js
+++ desafio-5/model/data.js
@@ -2,21 +2,21 @@ const products = [
   /*  {
     id: 1,
     title: "Manzana",
-    price: 123.44,
+    price: 130,
     thumbnail:
       "https://elegifruta.com.ar/onepage/wp-content/uploads/2017/07/manzana_roja.jpg",
   },
   {
     id: 2,
     title: "Pera",
-    price: 221.14,
+    price: 300,
     thumbnail:
       "https://perfumesyfragancias.online/wp-content/uploads/2018/10/poire.jpg",
   },
   {
     id: 3,
-    title: "Cambur",
-    price: 33.98,
+    title: "Banana",
+    price: 500,
     thumbnail:
       "https://wikidat.com/img/cambur-ff289f22159dbaeea6e8c88f3a4c94e5.jpg",
   }, */
